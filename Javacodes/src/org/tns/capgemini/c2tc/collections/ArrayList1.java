78d409b212c0077d214ce60e38f50170adf0d9fa
Nehabv99 <nehabv99@gmail.com>
2026-02-03 01:11:23 +0530

    Added ArrayListExamples

--- Javacodes/src/org/tns/capgemini/c2tc/collections/ArrayList1.java
+++ Javacodes/src/org/tns/capgemini/c2tc/collections/ArrayList1.java
@@ -8,6 +8,7 @@ public class ArrayList1 {
 
 	public static void main(String[] args) {
 		List<String> fruits=new ArrayList<>();
+		
 		fruits.add("Apple"); //add elements
 		fruits.add("Mango");
 		fruits.add("Dragon fruit");
